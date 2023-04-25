7434befc7be30014b16e0817b5f8e18859272520
Naoya Matayoshi <nanophate@users.noreply.github.com>
2023-04-25 15:23:04 +0900

    Update network.sh

--- src/scripts/network.sh
+++ src/scripts/network.sh
@@ -3,5 +3,7 @@ cat /proc/net/dev > network_data
 recive_bytes=$(awk '{print $1,$2}' network_data | grep eth0 | awk '{print $2}')    # Receive
 transmit_bytes=$(awk '{print $1,$10}' network_data | grep eth0 | awk '{print $2}') # Transmit
 
+cat network_data
+
 echo "recive $(echo "$recive_bytes"     | awk '{byte =$1 /1024/1024 ; print byte " MB"}')"
 echo "transmit $(echo "$transmit_bytes" | awk '{byte =$1 /1024/1024 ; print byte " MB"}')"
